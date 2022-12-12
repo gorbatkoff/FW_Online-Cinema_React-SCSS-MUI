853ec0666a54c4dfc0b5ce7aa98df7d2597f82d6
gorbatkoff <gorbatkovartyom@gmail.com>
2022-12-12 15:58:24 +0300

    Add preloader and films rendering

--- src/components/IndexContent/IndexContent.jsx
+++ src/components/IndexContent/IndexContent.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
+
 import { Container, Divider, Typography } from '@mui/material';
 
-import styles from './IndexContent.module.scss';
+import CardComponent from '../Card/CardComponent';
 
-function IndexContent() {
+import styles from './IndexContent.module.scss';
+import Prealoder from '../Preloader/Prealoder';
+
+function IndexContent({ films }) {
+
+  function renderFilms(films) {
+    return films.map((film) => {
+      console.log(film)
+      return <CardComponent film={film}/>
+    })
+  }
+
   return (
     <Container className={styles['index-container']}>
       <h1>Новости</h1>
 
       <h2>Будь всегда в курсе событий!</h2>
+
+      <div className={styles.titles}>
+        {films ? renderFilms(films) : <Prealoder />}
+      </div>
     </Container>
   )
 }
