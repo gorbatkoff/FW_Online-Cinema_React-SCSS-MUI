5dea8f69700824e75453be9afb0d57a4b95461f3
gorbatkoff <gorbatkovartyom@gmail.com>
2022-12-12 15:59:21 +0300

    Add connection with WebSocket

--- src/components/VideoBlock/WatchPage.jsx
+++ src/components/VideoBlock/WatchPage.jsx
@@ -1,18 +1,51 @@
 import { Button } from "@mui/material";
 import { React, useState, useEffect } from "react";
+
 import Player from "../../Player";
+
+import styles from './WatchPage.module.scss';
+
+const ws = new WebSocket("ws://95.163.235.66:80/api/room/");
+ws.onopen = () => {
+  console.log('hello world')
+};
+
+ws.onmessage = (event) => {
+}
+
 
 export default function WatchPage() {
 
   useScript("./playerjs.js");
 
   return (
-    <div>
-      <div id="player"></div>
-      <Player id="player" file="https://plrjs.com/sample.mp4" />
-      <Button onClick={PlayNewVideo}>Начать новое видео</Button>
+    <div className={styles.player}>
+      
+      <div className={styles.video}>
+        <div id="player"></div>
+        <Player id="player" file="https://plrjs.com/sample.mp4" />
+      </div>
+      
+      <div className={styles.controls}>
+        <Button onClick={startPlayer} variant="contained">Начать</Button>
+        <Button onClick={stopPlayer} variant="contained">Остановить</Button>
+        <Button onClick={syncPlayer} variant="contained">Синхронизировать</Button>
+        <Button onClick={PlayNewVideo} variant="contained">Начать новое видео</Button>
+      </div>
     </div>
   )
+}
+
+function startPlayer(){
+  alert('Start')
+}
+
+function stopPlayer(){
+  alert('Stop')
+}
+
+function syncPlayer(){
+  alert('Sync')
 }
 
 function useScript(url) {
