6635e54b41c1cf2af065b484db4c8bf234790438
gorbatkoff <gorbatkovartyom@gmail.com>
2022-12-12 18:53:26 +0300

    Add styles for movie's rating

--- src/components/Card/CardComponent.jsx
+++ src/components/Card/CardComponent.jsx
@@ -3,6 +3,7 @@ import { React, useEffect } from 'react';
 import styles from './CardComponent.module.scss';
 
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 function CardComponent({ film }) {
 
@@ -18,21 +19,23 @@ function CardComponent({ film }) {
 
     return (
         <div className={styles.poster}>
-            <div className={styles['movie-image']}>
-                <img src={poster} alt="" />
-            </div>
+            <a href={"/about-film/" + film.id}>
+                <div className={styles['movie-image']}>
+                    <img src={poster} alt="" />
+                </div>
 
-            <div className={styles['movie-title']}>
-                {film.name}
-            </div>
+                <div className={styles['movie-title']}>
+                    {film.name}
+                </div>
 
-            <div className={styles['movie-genre']}>
-                <span style={{ color: "#6c757d" }}>Мелодрама, Комедия, {film.year}</span>
-            </div>
+                <div className={styles['movie-genre']}>
+                    <span style={{ color: "#6c757d" }}>Мелодрама, Комедия, {film.year}</span>
+                </div>
 
-            <div className={styles['movie-rating']}>
-                Рейтинг: <span className={rating > 8.5 ? 'gold' : rating > 7.5 ? 'green' : 'red'}>{rating}</span>
-            </div>
+                <div className={styles['movie-rating']}>
+                    Рейтинг: <span className={rating > 8.5 ? 'gold' : rating > 7.5 ? 'green' : 'red'}>{rating}</span>
+                </div>
+            </a>
         </div>
     )
 }
