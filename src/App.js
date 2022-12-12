1e6dc6f3e9c286dfd55994b3968ac43cdc9d18c9
gorbatkoff <gorbatkovartyom@gmail.com>
2022-12-12 16:02:05 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { React, useState, useEffect, useContext. useCallback } from "react";
+import { React, useState, useEffect, useContext, useCallback } from "react";
 import { Routes, Route, Link } from 'react-router-dom';
 
 import Header from './components/Header/Header';
