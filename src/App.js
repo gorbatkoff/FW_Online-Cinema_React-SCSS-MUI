72dfe823cd085e570890aa2024e9b41662ca7ac2
gorbatkoff <gorbatkovartyom@gmail.com>
2022-12-13 15:28:11 +0300

    Add dynamic pagination

--- src/App.js
+++ src/App.js
@@ -15,36 +15,65 @@ import Footer from "./components/Footer/Footer.tsx";
 export default function App() {
 
   const [films, setFilms] = useState([])
+  const [currentPage, setCurrentPage] = useState(1);
+  const [fetching, setFetching] = useState(true)
 
   const api = axios.create({
     baseURL: 'https://api.kinopoisk.dev/'
   });
 
-  const getData = useCallback(async () =>{
+  const getData = async () => {
+
+    console.log('fetching data')
+
     try {
 
-      const response =  await api.get('movie?token=KNPB2YP-RW0MH46-H4RVRAZ-CCFGZVF&search=381&field=id&search=382&field=id&search=383&field=id&search=384&field=id&search=385&field=id');
+      if (fetching) {
+        const response = await api.get(`movie?field=rating.kp&search=6-10&field=year&search=2017-2022&field=typeNumber&search=2&sortField=year&token=KNPB2YP-RW0MH46-H4RVRAZ-CCFGZVF&limit=20&page=${currentPage}`);
 
-      setFilms(response.data);
+        console.log(response.data.docs)
+
+        setFilms([...films, ...response.data.docs]);
+
+        setCurrentPage(prevState => prevState + 1);
+      }
 
     }
-
     catch (error) {
       alert('Error: ' + error.message);
     }
 
-  }, [])
+    finally{
+      setFetching(false);
+    }
+  }
+
+  const scrollHandler = (e) => {
+
+    if(e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight) < 100){
+      setFetching(true);
+    }
+
+  }
 
 
   useEffect(() => {
-    getData();
+    document.addEventListener('scroll', scrollHandler);
+
+    return function () {
+      document.removeEventListener('scroll', scrollHandler);
+    }
   }, [])
+
+  useEffect(() => {
+    getData();
+  }, [fetching])
 
   return (
     <div>
       <Header />
       <Routes>
-        <Route path="/" element={[<IndexContent films={films.docs} />]} />
+        <Route path="/" element={[<IndexContent films={films} />]} />
         <Route path="/video" element={[<WatchPage />]} />
         <Route path="/room" element={[<RoomPage />]} />
         <Route path="/about-film/:id" element={[<MoviePage />]} />
