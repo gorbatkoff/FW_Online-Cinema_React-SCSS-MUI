c64d231d69146a08411f28f11b686fee4927863e
gorbatkoff <gorbatkovartyom@gmail.com>
2022-12-16 14:28:36 +0300

    Add dynamic fetching of films

--- src/components/IndexContent/IndexContent.jsx
+++ src/components/IndexContent/IndexContent.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import { Container, Divider, Typography } from '@mui/material';
 
@@ -7,7 +7,66 @@ import CardComponent from '../Card/CardComponent';
 import styles from './IndexContent.module.scss';
 import Prealoder from '../Preloader/Prealoder';
 
-function IndexContent({ films }) {
+import axios from 'axios';
+
+function IndexContent() {
+
+  
+  const [films, setFilms] = useState([])
+  const [currentPage, setCurrentPage] = useState(1);
+  const [fetching, setFetching] = useState(true)
+
+  const api = axios.create({
+    baseURL: 'https://api.kinopoisk.dev/'
+  });
+
+  const getData = async () => {
+
+    console.log('fetching data')
+
+    try {
+
+      if (fetching) {
+        const response = await api.get(`movie?field=rating.kp&search=6-10&field=year&search=2017-2022&field=typeNumber&search=2&sortField=year&token=KNPB2YP-RW0MH46-H4RVRAZ-CCFGZVF&limit=20&page=${currentPage}`);
+
+        console.log(response.data.docs)
+
+        setFilms([...films, ...response.data.docs]);
+
+        setCurrentPage(prevState => prevState + 1);
+      }
+
+    }
+    catch (error) {
+      alert('Error: ' + error.message);
+    }
+
+    finally{
+      setFetching(false);
+    }
+  }
+
+  const scrollHandler = (e) => {
+
+    if(e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight) < 100){
+      setFetching(true);
+    }
+
+  }
+
+
+  useEffect(() => {
+    document.addEventListener('scroll', scrollHandler);
+
+    return function () {
+      document.removeEventListener('scroll', scrollHandler);
+    }
+  }, [])
+
+  useEffect(() => {
+    getData();
+  }, [fetching])
+
 
   function renderFilms(films) {
     return films.map((film) => {
