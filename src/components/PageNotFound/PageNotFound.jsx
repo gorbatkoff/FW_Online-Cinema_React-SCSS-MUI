3e9ab8075f9cfa91a9e8c77b465a8a6cd9bb76bd
gorbatkoff <gorbatkovartyom@gmail.com>
2022-12-17 03:02:43 +0300

    Create PageNotFound component

---
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PageNotFound() {
+    return (
+        <h1 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: "300px auto 500px auto" }}>
+            Page not found
+        </h1>
+    )
+}
+
+export default PageNotFound
