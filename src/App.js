a148d8dab6b87384f7684b7a5f0f741de52c3bb8
gorbatkoff <gorbatkovartyom@gmail.com>
2022-12-18 15:52:12 +0300

    Comment Footer

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ export default function App() {
         <Route path="/about-film/:id" element={[<MoviePage />]} />
         <Route path="*" element={[<PageNotFound />]} />
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
 
 
 
